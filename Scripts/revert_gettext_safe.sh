db2fd00bc7de08b8bc01c8d45f9169f1d125f303
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2025-08-03 12:02:02 +0800

    Delete Scripts/revert_gettext_safe.sh

--- Scripts/revert_gettext_safe.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd "$(dirname "$0")/../wrt"
-echo ">>> 回退到 gettext-full host 可正常构建的版本 ac9a97e..."
-git reset --hard ac9a97e
-echo ">>> 回退完成，当前 HEAD:"
-git log -1 --oneline
