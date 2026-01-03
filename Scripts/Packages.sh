9a89ce51ba2d6d62695c46744fea81252f25ee3a
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2026-01-03 12:31:04 +0800

    Add new package updates for luci-lib and taskd

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -203,6 +203,9 @@ UPDATE_PACKAGE "luci-app-quickstart" "kiddin9/kwrt-packages" "main" "pkg"
 
 # istore商店
 #UPDATE_PACKAGE "luci-app-store" "shidahuilang/openwrt-package" "Immortalwrt" "pkg"
+UPDATE_PACKAGE "luci-lib-xterm" "kiddin9/kwrt-packages" "main" "pkg"
+UPDATE_PACKAGE "taskd" "kiddin9/kwrt-packages" "main" "pkg"
+UPDATE_PACKAGE "luci-lib-taskd" "kiddin9/kwrt-packages" "main" "pkg"
 UPDATE_PACKAGE "luci-app-store" "kiddin9/kwrt-packages" "main" "pkg"
 
 # 统一文件共享
