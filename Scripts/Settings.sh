106f1d0fc84a0db89d2267209c48f302cc581ece
master-yun-yun <79097488+master-yun-yun@users.noreply.github.com>
2026-02-22 13:56:16 +0800

    Update DTS_PATH for Qualcomm platform adjustments

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -49,6 +49,14 @@ fi
 
 #高通平台调整
 DTS_PATH="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/"
+‎
+Original file line number	Diff line number	Diff line change
+@@ -45,7 +45,7 @@ if [ -n "$WRT_PACKAGE" ]; then
+fi
+
+#高通平台调整
+#DTS_PATH="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/"
+DTS_PATH="./target/linux/qualcommax/dts/"
 if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 	#取消nss相关feed
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
